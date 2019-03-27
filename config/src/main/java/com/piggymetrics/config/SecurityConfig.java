a9b573fea0468224c93975ee34eb8e3219a4bf8f
k <k@nerv.org>
2019-03-27 16:42:11 +0800

    remove docker dependence

--- config/src/main/java/com/piggymetrics/config/SecurityConfig.java
+++ config/src/main/java/com/piggymetrics/config/SecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
- * @author cdov
+ * @author basic auth settup, using the yml file password setting
  */
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
