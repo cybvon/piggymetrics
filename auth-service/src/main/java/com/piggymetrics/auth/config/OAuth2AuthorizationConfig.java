09d46382cb18a6ebddfe16793f15ce4c5abc42e4
k <k@nerv.org>
2019-07-01 17:24:55 +0800

    refactor MongoTokenStore

--- auth-service/src/main/java/com/piggymetrics/auth/config/OAuth2AuthorizationConfig.java
+++ auth-service/src/main/java/com/piggymetrics/auth/config/OAuth2AuthorizationConfig.java
@@ -1,11 +1,10 @@
 package com.piggymetrics.auth.config;
 
-import com.piggymetrics.auth.service.AuthClientDetailsService;
-import com.piggymetrics.auth.service.CustomUserDetailsService;
-import com.piggymetrics.auth.service.MongoTokenStore;
+import com.piggymetrics.auth.service.security.MongoClientDetailsService;
+import com.piggymetrics.auth.service.security.MongoTokenStoreService;
+import com.piggymetrics.auth.service.security.MongoUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -14,7 +13,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenStore;
 
 
 /**
@@ -29,15 +27,15 @@ public class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdap
     private AuthenticationManager authenticationManager;
 
     @Autowired
-    private CustomUserDetailsService userDetailsService;
+    private MongoUserDetailsService/*CustomUserDetailsService*/ userDetailsService;
 
     @Autowired
-    private AuthClientDetailsService authClientDetailsService;
+    private MongoClientDetailsService/*AuthClientDetailsService*/ authClientDetailsService;
 
 //    @Bean
-//    public TokenStore tokenStore() {        return new MongoTokenStore();    }
+//    public Token tokenStore() {        return new MongoTokenStore();    }
     @Autowired
-    public MongoTokenStore mongoTokenStore;
+    public MongoTokenStoreService mongoTokenStore;
 
     private final String NOOP_PASSWORD_ENCODE = "{noop}";
 
